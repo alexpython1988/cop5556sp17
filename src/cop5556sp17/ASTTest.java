6a4e90d12e147fbf911dd47517bd53a833150968
Xi Yang <yunxiache@gmail.com>
2017-02-28 23:56:58 -0500

    starter of hw4

--- src/cop5556sp17/ASTTest.java
+++ src/cop5556sp17/ASTTest.java
@@ -340,4 +340,267 @@ public class ASTTest {
 		System.out.println(b);
 	}
 	
+	@Test
+	public void testFactor04() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "abc";
+		Scanner scanner = new Scanner(input);
+		scanner.scan();
+		Parser parser = new Parser(scanner);
+		ASTNode ast = parser.expression();
+		assertEquals(IdentExpression.class, ast.getClass());
+	}
+
+	@Test
+	public void testFactor11() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "123";
+		Scanner scanner = new Scanner(input);
+		scanner.scan();
+		Parser parser = new Parser(scanner);
+		ASTNode ast = parser.expression();
+		assertEquals(IntLitExpression.class, ast.getClass());
+	}
+
+
+
+	@Test
+	public void testBinaryExpr02() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "1+abc";
+		Scanner scanner = new Scanner(input);
+		scanner.scan();
+		Parser parser = new Parser(scanner);
+		ASTNode ast = parser.expression();
+		assertEquals(BinaryExpression.class, ast.getClass());
+		BinaryExpression be = (BinaryExpression) ast;
+		assertEquals(IntLitExpression.class, be.getE0().getClass());
+		assertEquals(IdentExpression.class, be.getE1().getClass());
+		assertEquals(PLUS, be.getOp().kind);
+	}
+	
+	@Test
+	public void testArg() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "  (3, ident0, ident4 < 3) ";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.arg();
+		assertEquals(Tuple.class, ast.getClass()); // test class
+		Tuple arg = (Tuple) ast;
+		List<Expression> expList = arg.getExprList();
+		
+		assertEquals(3, expList.size());
+		
+		assertEquals(IntLitExpression.class, expList.get(0).getClass());
+		assertEquals(IdentExpression.class, expList.get(1).getClass());
+		assertEquals(BinaryExpression.class, expList.get(2).getClass());
+	}
+	
+	@Test
+	public void testBlock() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "  {ident1 -> show(3); integer ident2 ident3 <- 3+5; boolean ident5 } ";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.block();
+		assertEquals(Block.class, ast.getClass()); // test class
+		Block b = (Block) ast;
+		List<Dec> decList = b.getDecs();
+		List<Statement> stateList =  b.getStatements();
+		
+		assertEquals(2, decList.size());
+		assertEquals(2, stateList.size());
+		
+		// ?
+		assertEquals(KW_INTEGER, decList.get(0).getType().kind);
+		assertEquals(IDENT, decList.get(0).getIdent().kind);
+		assertEquals(KW_BOOLEAN, decList.get(1).getType().kind);
+		assertEquals(IDENT, decList.get(1).getIdent().kind);
+		
+		assertEquals(BinaryChain.class, stateList.get(0).getClass());
+		assertEquals(IdentChain.class, ((BinaryChain)stateList.get(0)).getE0().getClass());
+		assertEquals(ARROW, ((BinaryChain)stateList.get(0)).getArrow().kind);
+		assertEquals(FrameOpChain.class, ((BinaryChain)stateList.get(0)).getE1().getClass());
+		
+		assertEquals(AssignmentStatement.class, stateList.get(1).getClass());
+		assertEquals(IdentLValue.class, ((AssignmentStatement)stateList.get(1)).getVar().getClass());
+		assertEquals(BinaryExpression.class, ((AssignmentStatement)stateList.get(1)).getE().getClass());
+	}
+	
+	@Test
+	public void testProgram() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = " program0 url ident0, file ident1, integer ident2 {ident1 -> show(3); integer ident2 ident3 <- 3+5; boolean ident5 } ";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.parse();
+		assertEquals(Program.class, ast.getClass()); // test class
+		Program prom = (Program) ast;
+		ArrayList<ParamDec> PDList = prom.getParams();
+		assertEquals(3, PDList.size());
+		
+		assertEquals("program0", prom.getName());
+		assertEquals(ParamDec.class, PDList.get(0).getClass());
+		assertEquals(ParamDec.class, PDList.get(1).getClass());
+		assertEquals(ParamDec.class, PDList.get(2).getClass());
+		assertEquals(Block.class, prom.getB().getClass());
+	}
+	
+	@Test
+	public void testElem0() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "ident1 * ident2 / 3345 & true % false / screenwidth * screenheight";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.elem();
+		assertEquals(BinaryExpression.class, ast.getClass()); // test class
+		
+		BinaryExpression be = (BinaryExpression) ast;
+		assertEquals(BinaryExpression.class, be.getE0().getClass());
+		assertEquals(ConstantExpression.class, be.getE1().getClass());
+		assertEquals(TIMES, be.getOp().kind);
+	}
+	
+	@Test
+	public void testTerm0() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "ident1 * ident2 + 3783 & 2893 - screenwidth / screenheight | true * false";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.term();
+		assertEquals(BinaryExpression.class, ast.getClass()); // test class
+		
+		BinaryExpression be = (BinaryExpression) ast;
+		assertEquals(BinaryExpression.class, be.getE0().getClass());
+		assertEquals(BinaryExpression.class, be.getE1().getClass());
+		assertEquals(OR, be.getOp().kind);
+	}
+	
+	@Test
+	public void testExpression0() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "ident1 * ident2 + 3783 & 2893 < true * false | screenwidth / screenheight <= true * false | screenwidth / screenheight > true * false | screenwidth / screenheight >= true * false | screenwidth / screenheight == true * false | screenwidth / screenheight != true * false | screenwidth / screenheight";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.expression();
+		assertEquals(BinaryExpression.class, ast.getClass()); // test class
+		
+		BinaryExpression be = (BinaryExpression) ast;
+		assertEquals(BinaryExpression.class, be.getE0().getClass());
+		assertEquals(BinaryExpression.class, be.getE1().getClass());
+		assertEquals(NOTEQUAL, be.getOp().kind);
+	}
+
+	@Test
+	public void testParamDec() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "url ident321";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.paramDec();
+		assertEquals(ParamDec.class, ast.getClass()); // test class
+		
+		Dec d = (Dec) ast;
+		assertEquals(KW_URL, d.getType().kind);
+		assertEquals(IDENT, d.getIdent().kind);
+	}
+	
+	@Test
+	public void testDec3() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "integer ident0";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.dec();
+		assertEquals(Dec.class, ast.getClass()); // test class
+		
+		Dec d = (Dec) ast;
+		assertEquals(KW_INTEGER, d.getType().kind);
+		assertEquals(IDENT, d.getIdent().kind);
+	}
+	
+	@Test
+	public void testChain() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "ident0 -> blur(3+2) |-> gray";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.chain();
+		assertEquals(BinaryChain.class, ast.getClass()); // test class
+		
+		BinaryChain bc = (BinaryChain) ast;
+		assertEquals(BinaryChain.class, bc.getE0().getClass());
+		assertEquals(FilterOpChain.class, bc.getE1().getClass());
+		assertEquals(BARARROW, bc.getArrow().kind);
+	}
+	
+	@Test
+	public void testChainElm0() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "show(5+4+3, 5<3)";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.chainElem();
+		assertEquals(FrameOpChain.class, ast.getClass()); // test class
+		
+		FrameOpChain fc = (FrameOpChain) ast;
+		assertEquals(2, fc.getArg().getExprList().size());
+		assertEquals(BinaryExpression.class, fc.getArg().getExprList().get(0).getClass());
+		assertEquals(BinaryExpression.class, fc.getArg().getExprList().get(1).getClass());
+	}
+	@Test
+	public void testChainElm1() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "convolve";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.chainElem();
+		assertEquals(FilterOpChain.class, ast.getClass()); // test class
+		
+		FilterOpChain fc = (FilterOpChain) ast;
+		assertEquals(0, fc.getArg().getExprList().size());
+		assertEquals(true, fc.getArg().getExprList().isEmpty());
+	}
+	@Test
+	public void testChainElm2() throws IllegalCharException, IllegalNumberException, SyntaxException {
+		String input = "width(5*3-2%3, 5<3, ident0+5)";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.chainElem();
+		assertEquals(ImageOpChain.class, ast.getClass()); // test class
+		
+		ImageOpChain fc = (ImageOpChain) ast;
+		assertEquals(3, fc.getArg().getExprList().size());
+		assertEquals(BinaryExpression.class, fc.getArg().getExprList().get(0).getClass());
+		assertEquals(BinaryExpression.class, fc.getArg().getExprList().get(1).getClass());
+		assertEquals(BinaryExpression.class, fc.getArg().getExprList().get(2).getClass());
+	}
+	
+	@Test
+	public void testWhileStatement() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "while(id1 * id2 + 3 & 2 < true * false | screenwidth / screenheight) {}";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.statement();
+		
+		assertEquals(WhileStatement.class, ast.getClass()); // test class
+		
+		WhileStatement ws = (WhileStatement) ast;
+		assertEquals(BinaryExpression.class, ws.getE().getClass());
+		assertEquals(Block.class, ws.getB().getClass());
+	}
+	
+	@Test
+	public void testIfStatement() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "if(id1 * id2 + 3 & 2 < true * false | screenwidth / screenheight) {integer ident0 boolean ident1 image ident2 frame ident3}";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.statement();
+		
+		assertEquals(IfStatement.class, ast.getClass()); // test class
+		
+		IfStatement is = (IfStatement) ast;
+		assertEquals(BinaryExpression.class, is.getE().getClass());
+		assertEquals(Block.class, is.getB().getClass());
+	}
+	
+	@Test
+	public void testAssign() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "ident1 <- ident1 * ident2 + 3783 & 2893 < true * false | screenwidth / screenheight <= true * false | screenwidth / screenheight > true * false | screenwidth / screenheight >= true * false | screenwidth / screenheight == true * false | screenwidth / screenheight != true * false | screenwidth / screenheight;";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.statement();
+		
+		assertEquals(AssignmentStatement.class, ast.getClass()); // test class
+		
+		AssignmentStatement is = (AssignmentStatement) ast;
+		assertEquals(IdentLValue.class, is.getVar().getClass());
+		assertEquals(BinaryExpression.class, is.getE().getClass());
+	}
+	
+	@Test
+	public void testSleep() throws IllegalCharException, IllegalNumberException, SyntaxException {
+        String input = "sleep ident1 * ident2 + 3783 & 2893 < true * false | screenwidth / screenheight <= true * false | screenwidth / screenheight > true * false | screenwidth / screenheight >= true * false | screenwidth / screenheight == true * false | screenwidth / screenheight != true * false | screenwidth / screenheight;";
+        //String input = "sleep id2 + 23;";
+		Parser parser = new Parser(new Scanner(input).scan());
+		ASTNode ast = parser.statement();
+		
+		assertEquals(SleepStatement.class, ast.getClass()); // test class
+		
+		SleepStatement is = (SleepStatement) ast;
+		assertEquals(BinaryExpression.class, is.getE().getClass());
+	}
+	
 }
